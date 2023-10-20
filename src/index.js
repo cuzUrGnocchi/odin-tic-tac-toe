467fc920b86d91bc22bbe7b5d8aa6e88beb1ac55
gnocchi <gkussunoki@gmail.com>
2023-10-20 07:00:34 -0300

    Separate application logic from  DOM manipulation

--- src/index.js
+++ src/index.js
@@ -23,13 +23,50 @@ function updateStreakCount() {
 }
 
 function updateLeaderboard() {
+  function createEntry(entry) {
+    const row = document.createElement('tr');
+    const position = document.createElement('td');
+    const name = document.createElement('td');
+    const time = document.createElement('td');
+
+    row.appendChild(position);
+    row.appendChild(name);
+    row.appendChild(time);
+
+    position.classList.add('position');
+    name.classList.add('name');
+    time.classList.add('time');
+
+    const seconds = entry.time.seconds < 10 ? `0${entry.time.seconds}` : entry.time.seconds;
+    const centiSeconds = entry.time.centiSeconds < 10 ? `0${entry.time.centiSeconds}` : entry.time.centiSeconds;
+
+    position.textContent = entry.position;
+    name.textContent = entry.name;
+    time.textContent = `${entry.time.minutes}:${seconds}:${centiSeconds}`;
+
+    return row;
+  }
+
   const board = document.querySelector('.leaderboard');
+  const container = document.createElement('div');
+  const title = document.createElement('h2');
+  const table = document.createElement('table');
 
   if (board !== null) {
     board.remove();
   }
 
-  document.querySelector('.content > div').appendChild(leaderboard.generateDom());
+  container.appendChild(title);
+  container.appendChild(table);
+  for (let i = 0; i < 10 && i < leaderboard.entries.length; i += 1) {
+    table.appendChild(createEntry(leaderboard.entries[i]));
+  }
+
+  container.classList.add('leaderboard');
+
+  title.textContent = 'Leaderboard';
+
+  document.querySelector('.content > div').appendChild(container);
 }
 
 let handleClick;
--- src/leaderboard.js
+++ src/leaderboard.js
@@ -42,50 +42,12 @@ const leaderboard = {
       }));
   },
 
-  clear() {
-    entries = [];
+  get entries() {
+    return entries;
   },
 
-  generateDom() {
-    function createEntry(entry) {
-      const row = document.createElement('tr');
-      const position = document.createElement('td');
-      const name = document.createElement('td');
-      const time = document.createElement('td');
-
-      row.appendChild(position);
-      row.appendChild(name);
-      row.appendChild(time);
-
-      position.classList.add('position');
-      name.classList.add('name');
-      time.classList.add('time');
-
-      const seconds = entry.time.seconds < 10 ? `0${entry.time.seconds}` : entry.time.seconds;
-      const centiSeconds = entry.time.centiSeconds < 10 ? `0${entry.time.centiSeconds}` : entry.time.centiSeconds;
-
-      position.textContent = entry.position;
-      name.textContent = entry.name;
-      time.textContent = `${entry.time.minutes}:${seconds}:${centiSeconds}`;
-
-      return row;
-    }
-
-    const container = document.createElement('div');
-    const title = document.createElement('h2');
-    const table = document.createElement('table');
-
-    container.appendChild(title);
-    container.appendChild(table);
-    for (let i = 0; i < 10 && i < entries.length; i += 1) {
-      table.appendChild(createEntry(entries[i]));
-    }
-
-    container.classList.add('leaderboard');
-
-    title.textContent = 'Leaderboard';
-
-    return container;
+  clear() {
+    entries = [];
   },
 };
 
