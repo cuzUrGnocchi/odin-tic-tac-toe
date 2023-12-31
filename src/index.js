66695169f9f1fd5cc3047a4e3611578882de2189
gnocchi <gkussunoki@gmail.com>
2023-12-31 11:58:29 -0300

    Lay the groundwork for the main script

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import Game from './model/Game.js';
 import Board from './model/Board.js';
 
 import renderBoard from './view/Board.js';
@@ -5,17 +6,75 @@ import renderSidebar from './view/Sidebar.js';
 
 import './assets/styles.css';
 
+let game = new Game();
+let board = renderBoard(new Board());
 const sidebar = renderSidebar();
 
-document.body.appendChild(renderBoard(new Board()));
+document.body.appendChild(board);
 document.body.appendChild(sidebar);
+
+function checkWinner() {
+  if (game.board.winner || game.board.isFull) {
+    updateStreakCount();
+    if (game.getScore('Cpu') === 1) {
+      declareDefeat();
+    } else if (game.tieCount === 10) {
+      declareVictory();
+    } else {
+      game = game.reset();
+    }
+    updateBoard();
+  }
+}
+
+const executeCpuRoutine = () => {
+  if (game.players[game.playerIndex].isCpu) {
+    game = game.playTurn();
+    updateBoard();
+
+    requestAnimationFrame(() => {
+      checkWinner();
+
+      requestAnimationFrame(() => {
+        if (game.players[game.playerIndex].isCpu) {
+          executeCpuRoutine();
+        } else {
+          updateBoard();
+
+          requestAnimationFrame(() => {
+            document.querySelector('.board').addEventListener('click', handleClick, { once: true });
+          });
+        }
+      });
+    });
+  }
+};
+
+function handleBoardClick(event) {
+  game = game.playTurn(event.detail.x, event.detail.y);
+
+  const oldBoard = board;
+  board = renderBoard(game.board);
+  board.addEventListener('tileClicked', handleBoardClick);
+  oldBoard.replaceWith(board);
+
+  requestAnimationFrame(() => {
+    checkWinner();
+
+    requestAnimationFrame(executeCpuRoutine);
+  });
+}
 
 sidebar.addEventListener('startButtonClicked', () => {
   document.body.classList.add('in-game');
   document.body.classList.remove('pre-game');
+
+  board.addEventListener('tileClicked', handleBoardClick);
 });
 
 sidebar.addEventListener('retireButtonClicked', () => {
   document.body.classList.add('pre-game');
   document.body.classList.remove('in-game');
+
+  board.removeEventListener('tileClicked', handleBoardClick);
 });
